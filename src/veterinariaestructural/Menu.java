54052ac57a8517e122843302ae4e2427d33c35f8
Vinicius de Morais <viniciusmorais352@gmail.com>
2022-04-09 15:53:13 -0600

    Menu completo

--- src/veterinariaestructural/Menu.java
+++ src/veterinariaestructural/Menu.java
@@ -29,25 +29,25 @@ public class Menu {
                 MostrarMenuAtendimiento();
                 break;
             case 1:
+                MostrarMenuConsultas();
                 break;
             case 2:
+                MostrarMenuMedicaciones();
                 break;
             case 3:
+                MostrarMenuInventario();
                 break;
         }
     }
     
     private void MostrarMenuAtendimiento(){
-        try{
-            String[] opciones = { "Nueva Ficha", "Muestra Fila", "Salir" };
+        
+        String[] opciones = { "Nueva Ficha", "Muestra Fila", "Volver" };
 
-            opcion = JOptionPane.showOptionDialog(null,
-                    "Seleccione una opción:",
-                    "Atendimiento",
-            JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
-        }
-        //En caso de fallo o opcion de cancelar
-        catch (Exception ex) { opcion = 3; }
+        opcion = JOptionPane.showOptionDialog(null,
+                "Seleccione una opción:",
+                "Atendimiento",
+        JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
         
         switch(opcion){
             case 0:
@@ -61,16 +61,13 @@ public class Menu {
     }
     
     private void MostrarMenuConsultas(){
-        try{
-            String[] opciones = { "Atiende Fila", "Mostrar Fila", "Salir" };
+        
+        String[] opciones = { "Atiende Fila", "Mostrar Fila", "Volver" };
 
-            opcion = JOptionPane.showOptionDialog(null,
-                    "Seleccione una opción:",
-                    "Consultas",
-            JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
-        }
-        //En caso de fallo o opcion de cancelar
-        catch (Exception ex) { opcion = 3; }
+        opcion = JOptionPane.showOptionDialog(null,
+                "Seleccione una opción:",
+                "Consultas",
+        JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
         
         switch(opcion){
             case 0:
@@ -78,26 +75,21 @@ public class Menu {
             case 1:
                 break;
             case 2:
-                break;
-            case 3:
                 MostrarMenuPrincipal();
                 break;
         }
     }
     
+    //Mostrar un menu personalizado para cada animal que inicie consulta
     private void MostrarMenuConsultaActiva() {
-        
-        try{
-            String[] opciones = { "Crear Prescripción", "Solicitar Vacunación", "Terminar Consulta" };
 
-            opcion = JOptionPane.showOptionDialog(null,
-                    "Seleccione una opción:",
-                    "Consultas",
-            JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
-        }
-        //En caso de fallo o opcion de cancelar
-        catch (Exception ex) { opcion = 3; }
-        
+        String[] opciones = { "Crear Prescripción", "Solicitar Vacunación", "Terminar Consulta" };
+
+        opcion = JOptionPane.showOptionDialog(null,
+                "Seleccione una opción:",
+                "Consultas",
+        JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
+
         switch(opcion){
             case 0:
                 break;
@@ -112,16 +104,13 @@ public class Menu {
     }
     
     private void MostrarMenuMedicaciones(){
-        try{
-            String[] opciones = { "Entregar Medicación", "Vacunar Animal", "Salir" };
 
-            opcion = JOptionPane.showOptionDialog(null,
-                    "Seleccione una opción:",
-                    "Medicaciones",
-            JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
-        }
-        //En caso de fallo o opcion de cancelar
-        catch (Exception ex) { opcion = 3; }
+        String[] opciones = { "Entregar Medicación", "Vacunar Animal", "Volver" };
+
+        opcion = JOptionPane.showOptionDialog(null,
+                "Seleccione una opción:",
+                "Medicaciones",
+        JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
         
         switch(opcion){
             case 0:
@@ -130,6 +119,49 @@ public class Menu {
                 break;
             case 2:
                 MostrarMenuPrincipal();
+                break;
+        }
+    }
+    
+    private void MostrarMenuInventario(){
+
+        String[] opciones = { "Inventario Medicación", "Desechar Vacunas", "Volver" };
+
+        opcion = JOptionPane.showOptionDialog(null,
+                "Seleccione una opción:",
+                "Medicaciones",
+        JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
+        
+        switch(opcion){
+            case 0:
+                MostrarMenuInventarioDeMedicación();
+                break;
+            case 1:
+                break;
+            case 2:
+                MostrarMenuPrincipal();
+                break;
+        }
+    }
+    
+    private void MostrarMenuInventarioDeMedicación(){
+
+        String[] opciones = { "Añadir Medicación", "Modificar Medicación", "Mostrar Inventario", "Volver" };
+
+        opcion = JOptionPane.showOptionDialog(null,
+                "Seleccione una opción:",
+                "Medicaciones",
+        JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
+        
+        switch(opcion){
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                MostrarMenuInventario();
                 break;
         }
     }
